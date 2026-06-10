20c6516c0c8e51cfe7db7e41dbe012916c94428a
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2026-06-09 22:29:57 -0300

    iniciando projeto movie stream

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*==================== MENU SHOW & HIDDEN ====================*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDE =====*/
+/* Validate if constant exists */
+
+/*==================== SCROLL HEADER ====================*/
+
+/*==================== NAVBAR SEARCH ====================*/
