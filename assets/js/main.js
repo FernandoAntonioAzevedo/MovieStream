42a45eab046d543d3ccffdbec1a7b6ab35534c3f
fernandoabc22 <beratechsolutions@gmail.com>
2026-09-08 21:53:40 -0300

    finalizando header e estruturando trending sections + insercao de tubnails de movies

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,27 @@
 /* Validate if constant exists */
 
 /*==================== SCROLL HEADER ====================*/
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50 
+  ? header.classList.add('bg-header') 
+  : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== NAVBAR SEARCH ====================*/
+const navSearch = document.getElementById('nav-search'),
+    search = document.getElementById('search');
+
+    navSearch.addEventListener('click', () => {
+        search.classList.toggle('show-search');
+
+        if (navSearch.classList.contains('ri-search-line')) {
+          navSearch.classList.replace('ri-search-line', 'ri-close-line');
+        } else {
+          navSearch.classList.replace('ri-close-line', 'ri-search-line');  
+        }
+    });
+
