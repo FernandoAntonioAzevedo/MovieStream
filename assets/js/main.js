8217e81f5c64215ef7b069bbb167ac4a900c4184
fernandoabc22 <beratechsolutions@gmail.com>
2026-09-24 23:53:37 -0300

    finalizando as otimizacoes na responsividade e fechando as alteracoes de layout. Agora só preencher com todo o conteudo final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 /*==================== MENU SHOW & HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if(navToggle) {
+  navToggle.addEventListener('click', ()  => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 /*===== MENU HIDE =====*/
 /* Validate if constant exists */
+if(navClose) {
+  navClose.addEventListener('click', ()  => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*==================== SCROLL HEADER ====================*/
 const scrollHeader = () => {
